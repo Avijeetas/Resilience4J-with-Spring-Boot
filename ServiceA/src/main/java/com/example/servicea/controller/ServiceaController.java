df56607f38841213f7d8d48e1068258df95f3909
Avijeet Shil <avijeet.vivasoft@bkash.com>
2022-12-12 13:30:31 +0600

    adding counter and retry method

--- ServiceA/src/main/java/com/example/servicea/controller/ServiceaController.java
+++ ServiceA/src/main/java/com/example/servicea/controller/ServiceaController.java
@@ -1,11 +1,14 @@
 package com.example.servicea.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/a")
@@ -14,12 +17,12 @@ public class ServiceaController {
     private RestTemplate restTemplate;
     private static final String Base_URL="http://localhost:8083/";
     private static final String SERVICE_A="serviceA";
-
+    int count=1;
     @GetMapping
-    @CircuitBreaker(name = SERVICE_A, fallbackMethod = "serviceAFallback")
-
+ // @CircuitBreaker(name = SERVICE_A, fallbackMethod = "serviceAFallback")
+    @Retry(name = SERVICE_A)
     public String serviceA(){
-
+        System.out.println("retry called "+(++count)+" at "+new Date());
         String url = Base_URL + "/b";
         return  restTemplate.getForObject(url, String.class);
 
